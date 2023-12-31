ccf844bb43f699eab254678cbc7fc042d9ac5821
André (ugsto) Bortoli <andre.ugsto@proton.me>
2023-12-31 19:32:42 -0300

    fix: build does not support top-level await

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import { WhatsappDefinition } from './controllers/meta/whatsapp.js';
 import { getWhatsappController } from './controllers/whatsapp.js';
 import { WhatsWebJsLoader } from './third-parties/whats-web-js/loader.js';
 
-const server = createServer();
-const client = await new WhatsWebJsLoader().loadClient();
+async function main() {
+	const server = createServer();
+	const client = await new WhatsWebJsLoader().loadClient();
 
-server.add(WhatsappDefinition, getWhatsappController(client));
+	server.add(WhatsappDefinition, getWhatsappController(client));
 
-server
-	.listen('0.0.0.0:50051')
-	.then(() => {
-		console.log('gRPC server listening on 0.0.0.0:50051');
-	})
-	.catch(console.error);
+	server
+		.listen('0.0.0.0:50051')
+		.then(() => {
+			console.log('gRPC server listening on 0.0.0.0:50051');
+		})
+		.catch(console.error);
+}
+
+main().catch(console.error);
